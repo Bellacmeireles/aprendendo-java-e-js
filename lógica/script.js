197202fb98044e3d2d4f5c7a077aa28e660e194c
Isabella Cabral <bellacabral80@gmail.com>
2023-01-18 21:40:14 -0300

    atualizando

---
+++ lógica/script.js
@@ -0,0 +1,24 @@
+let score = 100;
+function getscore() {
+  let scoreA = score >= 90 && score <= 100;
+  let scoreB = score >= 80 && score <= 89;
+  let scoreC = score >= 70 && score <= 79;
+  let scoreD = score >= 60 && score <= 69;
+  let scoreE = score < 60 && score >= 0;
+
+  if (scoreA) {
+    scoreFinal = "A";
+  } else if (scoreB) {
+    scoreFinal = "B";
+  } else if (scoreC) {
+    scoreFinal = "C";
+  } else if (scoreD) {
+    scoreFinal = "D";
+  } else if (scoreE) {
+    scoreFinal = "E";
+  } else {
+    scoreFinal = "Nota Inválida";
+  }
+
+  console.log(scoreFinal);
+}
