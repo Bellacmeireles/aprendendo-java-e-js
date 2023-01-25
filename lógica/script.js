913ab4731f635840c594752e02bd1bdbf57364de
Isabella Cabral <bellacabral80@gmail.com>
2023-01-24 21:10:50 -0300

    exercícios¹

--- lógica/script.js
+++ lógica/script.js
@@ -54,7 +54,7 @@ prop do tipo array: * receitas: [] * despesas: []
 
 */
 
-let family = {
+/*let family = {
   receitas: [200, 3120, 250.33, 360.45],
   despesas: [320.34, 128.45, 176.87, 1450.00]
 }
@@ -86,4 +86,132 @@ function calculateBalance() {
 
 }
 
-calculateBalance()
+calculateBalance() */
+
+/* Celsius em fahrenheit
+
+1* Crie uma função que receba uma string em celsius ou fahrenheit e faça a transformação de uma unidade para outra 
+
+c = (F - 32) * 5/9
+F = C * 9/5 + 32
+
+
+function transformDegree(degree) {
+  const celsiusExist = degree.toUpperCase().includes('C')
+  const fahrenheitExist = degree.toUpperCase().includes('F')
+  
+  // fluxo de erro 
+  if(!celsiusExist && !fahrenheitExist) {
+    throw new Error('Grau não identificado')
+  }
+
+  // esse let form é um EX de "arrow function"
+  // fluxo ideal, F -> C /- ou seja, fahrenheit para celsius
+  let updatedDegree = Number(degree.toUpperCase().replace("F",""));
+  let form = (fahrenheit) => (fahrenheit - 32) * 5/9
+  let degreeSign = 'C'
+
+  // fluxo alternativo C -> F /- ou seja, celsius para fahrenheit
+  if(celsiusExist) {
+    updatedDegree = Number(degree.toUpperCase().replace("C",""));
+    form = (celsius) => (celsius * 9/5 + 32)
+    degreeSign = 'F'
+  }
+
+  return form(updatedDegree) + degreeSign 
+}
+
+try {
+  console.log(transformDegree('25C'))
+  console.log(transformDegree('50F'))
+  console.log(transformDegree('50Z'))
+  //transformDegree('50Z')
+} catch (error) {
+  console.log(error.message)
+}
+*/
+
+/* Buscando e contando dados em array 
+Baseado no Array de livros por Categoria abaixo, faça os seguintes desafios
+
+1- Contar o número de categorias e o número de livros em cada categoria
+
+2- Contar o número de autores 
+
+3- Mostrar livros do autor Augusto Cury
+
+4- Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor
+
+const booksByCategory = [
+  {
+    category: "Riqueza",
+    books:[
+      { 
+        title: "Os segredos da mente milionária",
+        author: "T. Harv Eker",
+      },
+      {
+        title: "O homem mais rico da Babilônia",
+        author: "George S. Clason",
+      },
+      {
+        title: "Pai rico, pai pobre",
+        author: "Robert T. Kiyosaki",
+      },
+    ],
+  },
+  {
+    category: "Inteligência Emocional",
+    books:[
+      { 
+        title: "Você é Insubstituível",
+        author: "Augusto Cury",
+      },
+      {
+        title: "Ansiedade - Como enfrentar o mal do século",
+        author: "Augusto Cury",
+      },
+      {
+        title: "Os 7 hábitos das pessoas altamente eficazes",
+        author: "Stephen R. Covey",
+      },
+    ],
+  },
+];
+
+const totalCategories = booksByCategory.length
+
+console.log(totalCategories);
+for(let category of booksByCategory) {
+  console.log('Total de livros da categoria: ', category.category)
+  console.log(category.books.length)
+}
+
+function countAuthors() {
+  let authors = [];
+
+  for(let category of booksByCategory) {
+    for(let book of category.books) {
+      if(authors.indexOf(book.author) == -1)
+      authors.push(book.title)
+    }
+  }
+  console.log("Total de autores: ", authors.length)
+}
+
+countAuthors();
+
+function booksOfAugustoCury(author) {
+  let books = [];
+
+  for(let category of booksByCategory) {
+    for(let book of category.books) {
+      if(book.author === author) {
+        books.push(book.title)
+      }
+    }
+  }
+  console.log(`Livros do autor ${author}:  ${books.join(", ")}`)
+}
+
+booksOfAugustoCury('Augusto Cury'); */
